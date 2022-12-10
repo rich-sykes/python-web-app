546ad68017ef355f416db8cf0a7366e22dfd1646
Richard Sykes <rich.sykes@hotmail.co.uk>
2022-12-10 20:50:55 +0000

    Fix contact paramter to dict.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI(version='0.0.0',
               title='FastAPI based WebApp!',
               description='Step by step walk through to building a Python web app!',
-              contact='https://github.com/rich-sykes'
+              contact={'GitHub': 'https://github.com/rich-sykes'},
               )
 
 
