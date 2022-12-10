e5389502ab47be2a08920f07638fb8327b725576
Richard Sykes <rich.sykes@hotmail.co.uk>
2022-12-10 20:53:57 +0000

    Set ReDoc as index url.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ app = FastAPI(version='0.0.0',
               title='FastAPI based WebApp!',
               description='Step by step walk through to building a Python web app!',
               contact={'GitHub': 'https://github.com/rich-sykes'},
+              docs_url=None,
+              redoc_url='/',
               )
 
 
