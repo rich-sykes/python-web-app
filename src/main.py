f487e5361b93eb3aad0e73a35754c15ae5e5cfcd
Richard Sykes <rich.sykes@hotmail.co.uk>
2022-12-10 12:02:38 +0000

    Add the API app meta data.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(version='0.0.0',
+              title='FastAPI based WebApp!',
+              description='Step by step walk through to building a Python web app!',
+              contact='https://github.com/rich-sykes'
+              )
 
 
 @app.get("/")
